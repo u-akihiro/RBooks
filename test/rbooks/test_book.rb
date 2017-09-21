5a454d085fde325418c52ba0d40099b16a56dc88
uesaka <uesaka@s.codia.co.jp>
2017-09-21 12:22:06 +0900

    add test

--- test/rbooks/test_book.rb
+++ test/rbooks/test_book.rb
@@ -4,8 +4,10 @@ require "uri"
 
 class TestBook < Test::Unit::TestCase
   def test_encode_each_param
-    params = Rbooks::Book.encode_each_params({title: ['なれるSE', 'こころ']})
+    params = Rbooks::Book.encode_each_params({title: ["なれるSE", "こころ"], author: ["夏目漱石", "与謝野晶子"]})
+
     assert_equal Hash, params.class
-    assert_equal params[:title], URI.encode_www_form_component('なれるSE こころ')
+    assert_equal params[:title], URI.encode_www_form_component("なれるSE こころ")
+    assert_equal params[:author], URI.encode_www_form_component("夏目漱石 与謝野晶子")
   end
 end
